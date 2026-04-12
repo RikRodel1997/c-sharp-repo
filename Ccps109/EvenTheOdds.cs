ba3875d3b5cfe08d92d060a223ad5bbb0080c59f
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2026-04-12 09:12:47 +0200

    feat(Ccps109): EvenTheOdds

---
+++ Ccps109/EvenTheOdds.cs
@@ -0,0 +1,23 @@
+namespace Ccps109;
+
+public class EvenTheOdds
+{
+    public static bool OnlyOddDigits(long n)
+    {
+        if (n == 0)
+        {
+            return false;
+        }
+        bool onlyOdd = true;
+        string str = n.ToString();
+        foreach (char c in str)
+        {
+            long number = Convert.ToInt64(new string(c, 1));
+            if (number % 2 == 0)
+            {
+                onlyOdd = false;
+            }
+        }
+        return onlyOdd;
+    }
+}
